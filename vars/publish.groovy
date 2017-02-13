66e66e83bfc6df5f8e3f99dab9606f5499f91cd0
Katelyn Gigante <clockwork.singularity@gmail.com>
2017-02-13 19:05:06 +1100

    Drop the second param

--- vars/publish.groovy
+++ vars/publish.groovy
@@ -1,6 +1,6 @@
-def call(String exe, String args){
+def call(String args){
      if (!fileExists('clickmono.exe')) {
          sh('curl http://ci.katelyngigante.com/job/silasary/job/clickmono/job/master/lastSuccessfulBuild/artifact/ClickMac/bin/Release/ClickMac.exe -o clickmono.exe')
      }
-     mono('clickmono.exe', '--packager ${exe} ${args}')
+     mono('clickmono.exe', '--packager ${args}')
 }
