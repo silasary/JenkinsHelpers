5aae9f1a04b22e5123edd70e4ff3c8de8d17e5f3
Katelyn Gigante <clockwork.singularity@gmail.com>
2017-02-13 19:36:56 +1100

    doublequotes

--- vars/publish.groovy
+++ vars/publish.groovy
@@ -2,5 +2,5 @@ def call(String args){
      if (!fileExists('clickmono.exe')) {
          sh('curl http://ci.katelyngigante.com/job/silasary/job/clickmono/job/master/lastSuccessfulBuild/artifact/ClickMac/bin/Release/ClickMac.exe -o clickmono.exe')
      }
-     mono('clickmono.exe', '--packager ${args}')
+     mono("clickmono.exe", "--packager ${args}")
 }
