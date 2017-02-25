339e6ba45a65df04ae02fd876fddb0500abb5dbd
Katelyn Gigante <clockwork.singularity@gmail.com>
2017-02-25 19:16:10 +1100

    add `msbuild.debug()`

--- vars/msbuild.groovy
+++ vars/msbuild.groovy
@@ -11,3 +11,17 @@ def call()
       bat 'msbuild /t:Build /p:Configuration=RELEASE'
     }
 }
+
+def debug()
+{
+    if (isUnix())
+    {
+        sh 'nuget restore'
+        sh 'xbuild /t:Build /p:Configuration=DEBUG'
+    }
+    else
+    {
+      bat 'nuget restore'
+      bat 'msbuild /t:Build /p:Configuration=DEBUG'
+    }
+}
