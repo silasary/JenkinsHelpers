a081588c6263d0e314099e600d99ab7446012c9d
Katelyn Gigante <clockwork.singularity@gmail.com>
2017-06-18 22:12:48 +1000

    Use VSWhere to find the location of newer msbuild versions

--- vars/msbuild.groovy
+++ vars/msbuild.groovy
@@ -8,7 +8,19 @@ def call()
     else
     {
       bat 'nuget restore'
-      bat 'msbuild /t:Build /p:Configuration=RELEASE'
+      bat '''set PATH="%PATH%;%ProgramFiles(x86)%\\Microsoft Visual Studio\\Installer\\"
+if not exist "%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" (
+    curl -O https://github.com/Microsoft/vswhere/releases/download/1.0.71/vswhere.exe
+)
+for /f "usebackq tokens=1* delims=: " %%i in (`vswhere -latest -products * -requires Microsoft.Component.MSBuild -property installationPath`) do (
+  if /i "%%i"=="installationPath" set InstallDir=%%j
+)
+
+set msbuild="msbuild"
+if exist "%InstallDir%\\MSBuild\\15.0\\Bin\\MSBuild.exe" (
+  set msbuild="%InstallDir%\\MSBuild\\15.0\\Bin\\MSBuild.exe"
+)
+%msbuild% /t:Build /p:Configuration=RELEASE'''
     }
 }
 
@@ -22,6 +34,18 @@ def debug()
     else
     {
       bat 'nuget restore'
-      bat 'msbuild /t:Build /p:Configuration=DEBUG'
+      bat '''set PATH="%PATH%;%ProgramFiles(x86)%\\Microsoft Visual Studio\\Installer\\"
+if not exist "%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" (
+    curl -O https://github.com/Microsoft/vswhere/releases/download/1.0.71/vswhere.exe
+)
+for /f "usebackq tokens=1* delims=: " %%i in (`vswhere -latest -products * -requires Microsoft.Component.MSBuild -property installationPath`) do (
+  if /i "%%i"=="installationPath" set InstallDir=%%j
+)
+
+set msbuild="msbuild"
+if exist "%InstallDir%\\MSBuild\\15.0\\Bin\\MSBuild.exe" (
+  set msbuild="%InstallDir%\\MSBuild\\15.0\\Bin\\MSBuild.exe"
+)
+%msbuild% /t:Build /p:Configuration=DEBUG'''
     }
 }
