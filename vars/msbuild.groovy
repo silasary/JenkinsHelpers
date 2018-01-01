ba75d29085398a8be9a4d3c35a40cc33d8b109d6
Katelyn Gigante <clockwork.singularity@gmail.com>
2018-01-01 22:03:02 +1100

    I did a dumb

--- vars/msbuild.groovy
+++ vars/msbuild.groovy
@@ -7,22 +7,22 @@ def call()
     }
     else
     {
-      bat 'nuget restore'
-      bat '''set PATH="%ProgramFiles(x86)%\\Microsoft Visual Studio\\Installer\\;%PATH%"
-set vswhere="%ProgramFiles(x86)%\\Microsoft Visual Studio\\Installer\\vswhere.exe"
-if not exist %vswhere% (
-    curl -O https://github.com/Microsoft/vswhere/releases/download/1.0.71/vswhere.exe
-    set vswhere="vswhere"
-)
-for /f "usebackq tokens=1* delims=: " %%i in (`vswhere -latest -products * -requires Microsoft.Component.MSBuild -property installationPath`) do (
-  if /i "%%i"=="installationPath" set InstallDir=%%j
-)
+        bat 'nuget restore'
+        bat '''set PATH="%ProgramFiles(x86)%\\Microsoft Visual Studio\\Installer\\;%PATH%"
+    set vswhere="%ProgramFiles(x86)%\\Microsoft Visual Studio\\Installer\\vswhere.exe"
+    if not exist %vswhere% (
+        curl -O https://github.com/Microsoft/vswhere/releases/download/1.0.71/vswhere.exe
+        set vswhere="vswhere"
+    )
+    for /f "usebackq tokens=1* delims=: " %%i in (`%vswhere% -latest -products * -requires Microsoft.Component.MSBuild -property installationPath`) do (
+    if /i "%%i"=="installationPath" set InstallDir=%%j
+    )
 
-set msbuild="msbuild"
-if exist "%InstallDir%\\MSBuild\\15.0\\Bin\\MSBuild.exe" (
-  set msbuild="%InstallDir%\\MSBuild\\15.0\\Bin\\MSBuild.exe"
-)
-%msbuild% /t:Build /p:Configuration=RELEASE'''
+    set msbuild="msbuild"
+    if exist "%InstallDir%\\MSBuild\\15.0\\Bin\\MSBuild.exe" (
+    set msbuild="%InstallDir%\\MSBuild\\15.0\\Bin\\MSBuild.exe"
+    )
+    %msbuild% /t:Build /p:Configuration=RELEASE'''
     }
 }
 
