72d3e196b8e8c8c83de783415b74b73e851914ec
Katelyn Gigante <clockwork.singularity@gmail.com>
2018-01-01 22:17:54 +1100

    Further improve msbuild process

---
+++ resources/msbuild.cmd
@@ -0,0 +1,28 @@
+@REM Copyright (C) Microsoft Corporation. All rights reserved.
+@REM Licensed under the MIT license. See LICENSE.txt in the project root for license information.
+
+@if not defined _echo echo off
+setlocal enabledelayedexpansion
+
+@REM Determine if MSBuild is already in the PATH
+for /f "usebackq delims=" %%I in (`where msbuild.exe 2^>nul`) do (
+    "%%I" %*
+    exit /b !ERRORLEVEL!
+)
+
+set vswhere="%ProgramFiles(x86)%\\Microsoft Visual Studio\\Installer\\vswhere.exe"
+if not exist %vswhere% (
+    curl -O https://github.com/Microsoft/vswhere/releases/download/1.0.71/vswhere.exe
+    set vswhere="vswhere"
+)
+
+@REM Find the latest MSBuild that supports our projects
+for /f "usebackq delims=" %%I in (`%vswhere% -version "[15.0,)" -latest -prerelease -products * -requires Microsoft.Component.MSBuild Microsoft.VisualStudio.Component.Roslyn.Compiler Microsoft.VisualStudio.Component.VC.140 -property InstallationPath`) do (
+    for /f "usebackq delims=" %%J in (`where /r "%%I\MSBuild" msbuild.exe 2^>nul ^| sort /r`) do (
+        "%%J" %*
+        exit /b !ERRORLEVEL!
+    )
+)
+
+echo Could not find msbuild.exe 1>&2
+exit /b 2
--- vars/msbuild.groovy
+++ vars/msbuild.groovy
@@ -8,21 +8,9 @@ def call()
     else
     {
         bat 'nuget restore'
-        bat '''set PATH="%ProgramFiles(x86)%\\Microsoft Visual Studio\\Installer\\;%PATH%"
-    set vswhere="%ProgramFiles(x86)%\\Microsoft Visual Studio\\Installer\\vswhere.exe"
-    if not exist %vswhere% (
-        curl -O https://github.com/Microsoft/vswhere/releases/download/1.0.71/vswhere.exe
-        set vswhere="vswhere"
-    )
-    for /f "usebackq tokens=1* delims=: " %%i in (`%vswhere% -latest -products * -requires Microsoft.Component.MSBuild -property installationPath`) do (
-    if /i "%%i"=="installationPath" set InstallDir=%%j
-    )
-
-    set msbuild="msbuild"
-    if exist "%InstallDir%\\MSBuild\\15.0\\Bin\\MSBuild.exe" (
-    set msbuild="%InstallDir%\\MSBuild\\15.0\\Bin\\MSBuild.exe"
-    )
-    %msbuild% /t:Build /p:Configuration=RELEASE'''
+        msbuildcmd = libraryResource 'msbuild.cmd'
+        writeFile file: 'msbuild.cmd', text: msbuildcmd
+        bat 'msbuild /t:Build /p:Configuration=RELEASE'
     }
 }
 
@@ -35,19 +23,9 @@ def debug()
     }
     else
     {
-      bat 'nuget restore'
-      bat '''set PATH="%PATH%;%ProgramFiles(x86)%\\Microsoft Visual Studio\\Installer\\"
-if not exist "%ProgramFiles(x86)%\\Microsoft Visual Studio\\Installer\\vswhere.exe" (
-    curl -O https://github.com/Microsoft/vswhere/releases/download/1.0.71/vswhere.exe
-)
-for /f "usebackq tokens=1* delims=: " %%i in (`vswhere -latest -products * -requires Microsoft.Component.MSBuild -property installationPath`) do (
-  if /i "%%i"=="installationPath" set InstallDir=%%j
-)
-
-set msbuild="msbuild"
-if exist "%InstallDir%\\MSBuild\\15.0\\Bin\\MSBuild.exe" (
-  set msbuild="%InstallDir%\\MSBuild\\15.0\\Bin\\MSBuild.exe"
-)
-%msbuild% /t:Build /p:Configuration=DEBUG'''
+        bat 'nuget restore'
+        msbuildcmd = libraryResource 'msbuild.cmd'
+        writeFile file: 'msbuild.cmd', text: msbuildcmd
+        bat 'msbuild /t:Build /p:Configuration=DEBUG'''
     }
 }
